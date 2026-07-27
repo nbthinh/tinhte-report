f9bfc75ad7a9462292e047339191b6e2398b8c7d
Thinh Nguyen <nguyenbuuthinh036@gmail.com>
2026-07-27 15:26:36 +0700

    amamam

--- src/utils/report-data.ts
+++ src/utils/report-data.ts
@@ -1,6 +1,18 @@
 export const reportListId = [
     
     {
+        postId: "67231390",
+        reportReason: "Xúc phạm",
+        posterName: "",
+        url: "https://tinhte.vn/thread/hom-nay-co-dam-gio.4128907/"
+    },
+    {
+        postId: "67231360",
+        reportReason: "Kích động cãi nhau",
+        posterName: "",
+        url: "https://tinhte.vn/thread/hom-nay-co-dam-gio.4128907/"
+    },
+    {
         postId: "67230940",
         reportReason: "Kích động cãi nhau",
         posterName: "",
